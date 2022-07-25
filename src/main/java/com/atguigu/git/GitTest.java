f4d6891f706527181a947bc5d793149c7c51063b
zhouboan <13248150931@163.com>
2022-07-25 08:18:12 +0800

    commit  first

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.println("pulll test");
+        System.out.println("pull test");
     }
 }
